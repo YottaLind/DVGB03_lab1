5d504c992d8162948bbacad83690e57e6e624c46
YottaLind <yotta.lind@gmail.com>
2020-11-09 13:12:27 +0100

    Update algorithm.c

--- algorithm.c
+++ algorithm.c
@@ -10,6 +10,19 @@
 void bubble_sort(int *a, int n)
 {
 	// TODO: bubble sort
+	int temp, i, j;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
 }
 
 void insertion_sort(int *a, int n)
