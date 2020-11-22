34573ff7a0d0f040dcaa7a83a61ccd2537055bf6
monkcs <charlie.habolin@monkcs.se>
2020-11-22 16:21:53 +0100

    Fix bug in insertion sort

--- algorithm.c
+++ algorithm.c
@@ -29,7 +29,7 @@ void insertion_sort(int* array, int lenght)
 		const int key = array[i];
 		int j = i - 1;
 
-		while (j >= 0 && array[i] > key)
+		while (j >= 0 && array[j] > key)
 		{
 			array[j + 1] = array[j];
 			j--;
